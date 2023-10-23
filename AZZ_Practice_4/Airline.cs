b2daad0e12f9d7216585f173d1f562864ee76ecd
user2 <user2@DESKTOP-7J13RTJ>
2023-10-23 13:28:22 +0700

    Начало pr 4

---
+++ AZZ_Practice_4/Airline.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AZZ_Practice_4
+{
+    internal class Airline
+    {
+        private string? _destination;
+        public string? Destination 
+        { 
+            get { return _destination; } 
+            set 
+            {
+                if (string.IsNullOrEmpty(value)) return;
+                _destination = value; 
+            }
+        }
+
+        private int _flightNumber;
+        public int FlightNumber
+        {
+            get { return _flightNumber; }
+            set
+            {
+                if (value <= 0) return; 
+                _flightNumber = value;
+            }
+        }
+
+        
+    }
+}
---
+++ AZZ_Practice_4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace AZZ_Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([0-1][0-9]|2[0-4]):[0-5][0-9]";
+            string Air = "24:h4";
+
+            Console.WriteLine(Regex.IsMatch(Air, pattern) && Air.Length == 5);
+
+            //string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            //int L = 50;
+
+
+
+            //Console.Write("Введите длину строки, которая будет равна длине месяца: ");
+            //int.TryParse(Console.ReadLine(), out int n);
+
+            //var monthsByLenght = from m in months
+            //                     where m.Length == n
+            //                     select m;
+
+            //Console.WriteLine($"Месяцы длина которых равна {n}:");
+            //foreach (var item in monthsByLenght) Console.WriteLine(item);
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(new string('-', L));
+
+
+            //var monthsBySeasons = from m in months
+            //                      where m.Equals("June") || m.Equals("July") || m.Equals("August")
+            //                      || m.Equals("December") || m.Equals("January") || m.Equals("February")
+            //                      select m;
+
+            //Console.WriteLine("Летние и зимние месяцы:");
+            //foreach (var item in monthsBySeasons) Console.WriteLine(item);
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(new string('-', L));
+
+
+            //var monthsByAlphabet = from m in months
+            //                       orderby m
+            //                       select m;
+
+            //Console.WriteLine("Месяцы в алфавитном порядке:");
+            //foreach (var item in monthsByAlphabet) Console.WriteLine(item);
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(new string('-', L));
+
+
+            //var monthsByLetterLenght = from m in months
+            //                           where m.Contains("u") && m.Length >= 4
+            //                           select m;
+
+            //Console.WriteLine("Количество месяцев содержащих <u> и длиной не менее 4-х");
+            //Console.WriteLine(monthsByLetterLenght.Count());
+            //Console.WriteLine();
+        }
+    }
+}

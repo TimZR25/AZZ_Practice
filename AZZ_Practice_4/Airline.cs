73260178d3ba74a38d085c8b6278a89e0aefc4b0
Alex <7zaytsev00@mail.ru>
2023-10-23 16:17:32 +0700

    Практика 4

--- AZZ_Practice_4/Airline.cs
+++ AZZ_Practice_4/Airline.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AZZ_Practice_4;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace AZZ_Practice_4
 {
     internal class Airline
     {
+        private string pattern = @"([0-1][0-9]|2[0-4]):[0-5][0-9]";
+
         private string? _destination;
         public string? Destination 
         { 
@@ -31,6 +34,38 @@ namespace AZZ_Practice_4
             }
         }
 
-        
+        private string? departureTime;
+
+        private string? dayOfTheWeek;
+        public string? DepartureTime
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value)) return;
+                if (Regex.IsMatch(value, pattern) && value.Length == 5)
+                {
+                    departureTime = value;
+                }
+                else { throw new Exception("Неправильно введено время вылета"); }
+            }
+            get { return departureTime; }
+        }
+        public string? DayOfTheWeek { 
+            set { 
+                if (string.IsNullOrEmpty(value)) return;
+                dayOfTheWeek = value; 
+            } get { return dayOfTheWeek; } }
+
+
+
+        public Airline(string destination, int flightNumber, string departureTime, string dayOfTheWeek)
+        {
+            Destination = destination;
+            FlightNumber = flightNumber;
+            DepartureTime = departureTime;
+            DayOfTheWeek = dayOfTheWeek;
+        }
+
+        public override string ToString() { return $"Отправление в {Destination} в {DayOfTheWeek} {DepartureTime} по рейсу с номером: {FlightNumber}\n"; }
     }
 }
--- AZZ_Practice_4/Program.cs
+++ AZZ_Practice_4/Program.cs
@@ -7,63 +7,113 @@ namespace AZZ_Practice_4
     {
         static void Main(string[] args)
         {
-            string pattern = @"([0-1][0-9]|2[0-4]):[0-5][0-9]";
-            string Air = "24:h4";
-
-            Console.WriteLine(Regex.IsMatch(Air, pattern) && Air.Length == 5);
-
-            //string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-            //int L = 50;
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int L = 50;
 
 
 
-            //Console.Write("Введите длину строки, которая будет равна длине месяца: ");
-            //int.TryParse(Console.ReadLine(), out int n);
+            Console.Write("Введите длину строки, которая будет равна длине месяца: ");
+            int.TryParse(Console.ReadLine(), out int n);
 
-            //var monthsByLenght = from m in months
-            //                     where m.Length == n
-            //                     select m;
+            var monthsByLenght = from m in months
+                                 where m.Length == n
+                                 select m;
 
-            //Console.WriteLine($"Месяцы длина которых равна {n}:");
-            //foreach (var item in monthsByLenght) Console.WriteLine(item);
-            //Console.WriteLine();
-
-
-            //Console.WriteLine(new string('-', L));
+            Console.WriteLine($"Месяцы длина которых равна {n}:");
+            foreach (var item in monthsByLenght) Console.WriteLine(item);
+            Console.WriteLine();
 
 
-            //var monthsBySeasons = from m in months
-            //                      where m.Equals("June") || m.Equals("July") || m.Equals("August")
-            //                      || m.Equals("December") || m.Equals("January") || m.Equals("February")
-            //                      select m;
-
-            //Console.WriteLine("Летние и зимние месяцы:");
-            //foreach (var item in monthsBySeasons) Console.WriteLine(item);
-            //Console.WriteLine();
+            Console.WriteLine(new string('-', L));
 
 
-            //Console.WriteLine(new string('-', L));
+            var monthsBySeasons = from m in months
+                                  where m.Equals("June") || m.Equals("July") || m.Equals("August")
+                                  || m.Equals("December") || m.Equals("January") || m.Equals("February")
+                                  select m;
+
+            Console.WriteLine("Летние и зимние месяцы:");
+            foreach (var item in monthsBySeasons) Console.WriteLine(item);
+            Console.WriteLine();
 
 
-            //var monthsByAlphabet = from m in months
-            //                       orderby m
-            //                       select m;
-
-            //Console.WriteLine("Месяцы в алфавитном порядке:");
-            //foreach (var item in monthsByAlphabet) Console.WriteLine(item);
-            //Console.WriteLine();
+            Console.WriteLine(new string('-', L));
 
 
-            //Console.WriteLine(new string('-', L));
+            var monthsByAlphabet = from m in months
+                                   orderby m
+                                   select m;
+
+            Console.WriteLine("Месяцы в алфавитном порядке:");
+            foreach (var item in monthsByAlphabet) Console.WriteLine(item);
+            Console.WriteLine();
 
 
-            //var monthsByLetterLenght = from m in months
-            //                           where m.Contains("u") && m.Length >= 4
-            //                           select m;
+            Console.WriteLine(new string('-', L));
 
-            //Console.WriteLine("Количество месяцев содержащих <u> и длиной не менее 4-х");
-            //Console.WriteLine(monthsByLetterLenght.Count());
-            //Console.WriteLine();
+
+            var monthsByLetterLenght = from m in months
+                                       where m.Contains("u") && m.Length >= 4
+                                       select m;
+
+            Console.WriteLine("Количество месяцев содержащих <u> и длиной не менее 4-х");
+            Console.WriteLine(monthsByLetterLenght.Count());
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', L));
+
+            List<Airline> airlines = new List<Airline>() { new Airline("Astana", 1111, "14:15", "Monday"),
+                new Airline("Moscow", 2222, "15:50", "Friday"), new Airline("Astana", 1112, "14:30", "Monday"), new  Airline("Париж", 1113, "00:00", "Monday"),
+                new Airline("Novosibirsk", 3333, "22:58", "Wednesday")};
+
+
+            string dest = "Astana";
+            var airlines1 = from airline in airlines
+                            where airline.Destination.Equals(dest)
+                            select airline;
+
+            Console.WriteLine("Количество рейсов в " + dest + ": " + airlines1.Count());
+
+            Console.WriteLine(new string('-', L));
+
+            string day = "Monday";
+
+            var airlines2 = from airline in airlines
+                            where airline.DayOfTheWeek.Equals(day)
+                            select airline;
+
+            Console.WriteLine("Количество рейсов в " + day + ": " + airlines2.Count());
+
+            Console.WriteLine(new string('-', L));
+
+            var airlines3 = from airline in airlines
+                            where airline.DayOfTheWeek.Equals("Monday")
+                            orderby airline.DepartureTime
+                            select airline;
+
+            foreach (var item in airlines3) Console.WriteLine(item);
+
+            Console.WriteLine("\nСаммый ранний рейс в понедельник отправляется в: " + airlines3.First().DepartureTime);
+
+            Console.WriteLine(new string('-', L));
+
+            var airlines4 = from airline in airlines
+                            where (airline.DayOfTheWeek.Equals("Friday") || airline.DayOfTheWeek.Equals("Wednesday"))
+                            orderby airline.DepartureTime descending
+                            select airline;
+
+            foreach (var item in airlines4) Console.WriteLine(item);
+
+            Console.WriteLine("\nСаммый поздний рейс в пятницу или среду отправляется в: " + airlines4.First().DepartureTime);
+
+            Console.WriteLine(new string('-', L));
+
+            var airlines5 = from airline in airlines
+                            orderby airline.DepartureTime
+                            select airline;
+
+            Console.WriteLine("Отсортированный список рейсов по времени вылета:\n");
+            foreach (var item in airlines5) Console.WriteLine(item);
         }
     }
 }
